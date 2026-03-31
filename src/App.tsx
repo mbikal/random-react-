3bbb0fe360cb70aa1614a0cf0c4d2a33fe4d051c
mbikal <mbikal85@gmail.com>
2026-03-31 21:21:54 +0545

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
+import { useEffect, useState } from 'react';
 export default function App() {
+  
+  const [advice, setAdvice] = useState('');
+  const [count, setCount] = useState(0);
   async function getAdvice() {
-    const advice = await fetch('https://api.adviceslip.com/advice');
-    const data = await advice.json();
-    console.log(data);
+    const res = await fetch('https://api.adviceslip.com/advice');
+    const data = await res.json();
+    console.log(data.slip.advice);
+    setAdvice(data.slip.advice);
+    setCount((count) => count + 1);
   }
+  useEffect(function() {
+    getAdvice();
+  }, []);
   return (
     <div>
-      <h1>Hello, World!</h1>
+      <h1>{advice}</h1>
       <button onClick={getAdvice}>Get Advice</button>
+      <Message count={count} />
     </div>
   );
+}
+
+function Message(props: {count: number}) {
+  return (<p>We have read <strong>{props.count}</strong> adivces</p>);
 }
